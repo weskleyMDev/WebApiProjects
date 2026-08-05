436d042280d81ae3436cad1059f527a5e5d74246
Weskley Moreira <weskleymdev@gmail.com>
2026-08-05 20:18:56 -0300

    refactor(products): improve POST and GET methods

--- CatalogoMinimal/CatalogoAPIMinimal/Program.cs
+++ CatalogoMinimal/CatalogoAPIMinimal/Program.cs
@@ -82,11 +82,25 @@ app.MapDelete("/categories/{id:int}", async (int id, AppDbContext dataBase) =>
 
 app.MapPost("/products", async ([FromBody] Product product, [FromServices] AppDbContext dataBase) =>
 {
+    var categoryExists = await dataBase.Categories.AsNoTracking().AnyAsync(c => c.CategoryId == product.CategoryId);
+
+    if (!categoryExists)
+    {
+        return Results.NotFound(new
+        {
+            message = $"Category ID = {product.CategoryId} not found!"
+        });
+    }
+
     dataBase.Products.Add(product);
     await dataBase.SaveChangesAsync();
-    return Results.Created($"/products/{product.ProductId}", product);
+    return Results.CreatedAtRoute("GetProductById", new
+    {
+        id = product.ProductId
+    }, product);
 }).Accepts<Product>("application/json")
   .Produces<Product>(StatusCodes.Status201Created)
+  .Produces(StatusCodes.Status404NotFound)
   .WithName("CreateProduct")
   .WithTags("Products");
 
@@ -98,8 +112,54 @@ app.MapGet("/products", async (AppDbContext dataBase) =>
 
 app.MapGet("/products/{id:int}", async (int id, AppDbContext dataBase) =>
 {
-    var product = await dataBase.Products.AsNoTracking().FirstOrDefaultAsync(c => c.ProductId == id);
-    return product is not null ? Results.Ok(product) : Results.NotFound($"Product with ID = {id} not found!");
+    var product = await dataBase.Products.AsNoTracking().FirstOrDefaultAsync(p => p.ProductId == id);
+    return product is not null ? Results.Ok(product) : Results.NotFound(new
+    {
+        message = $"Product with ID = {id} not found!"
+    });
+}).Produces<Product>(StatusCodes.Status200OK)
+  .Produces(StatusCodes.Status404NotFound)
+  .WithName("GetProductById")
+  .WithTags("Products");
+
+app.MapPut("/products/{id:int}", async (int id, Product product, AppDbContext dataBase) =>
+{
+    if (product.ProductId != id)
+    {
+        return Results.BadRequest("Product ID mismatch!");
+    }
+
+    var updatedProduct = await dataBase.Products.FindAsync(id);
+
+    if (updatedProduct is null)
+    {
+        return Results.NotFound($"Product with ID = {id} not found!");
+    }
+
+    updatedProduct.Name = product.Name;
+    updatedProduct.Description = product.Description;
+    updatedProduct.Price = product.Price;
+    updatedProduct.ImageUrl = product.ImageUrl;
+    updatedProduct.Stock = product.Stock;
+    updatedProduct.CreatedAt = DateTime.UtcNow;
+    updatedProduct.CategoryId = product.CategoryId;
+
+    await dataBase.SaveChangesAsync();
+    return Results.Ok(updatedProduct);
+}).WithTags("Products");
+
+app.MapDelete("/products/{id:int}", async (int id, AppDbContext dataBase) =>
+{
+    var product = await dataBase.Products.FindAsync(id);
+
+    if (product is null)
+    {
+        return Results.NotFound($"Product with ID = {id} not found!");
+    }
+
+    dataBase.Products.Remove(product);
+    await dataBase.SaveChangesAsync();
+    return Results.NoContent();
 }).WithTags("Products");
 
 // Configure the HTTP request pipeline.
